1c7b4762986d90b70b12e5d003ed795146480a11
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-01-23 20:45:07 +0530

    feat: add basic structure for the app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>My Todos</h1>
+      <div className="todo-wrapper">
+        <div className="input-area">
+          <div className="input-items">
+            <label>Title</label>
+            <input type="text" placeholder="What's the task title?"/>
+          </div>
+          <div className="input-items">
+            <label>Description</label>
+            <input type="text" placeholder="What's the task description?"/>
+          </div>
+          <div className="input-items">
+            <button className="input-add-btn">Add</button>
+          </div>
+        </div>
+
+        <div className="btn-area">
+          <button>Todo</button>
+          <button>Completed</button>
+        </div>
+
+        <div className="todo-list-area">
+          <div className="todo-list-item">
+            <h3>Task 1</h3>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
